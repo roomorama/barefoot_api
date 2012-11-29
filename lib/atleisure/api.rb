2745f48c1350d7417836aa063f555ef3d047fb01
Nicolas Gaivironsky <ngaivi@gmail.com>
2012-11-29 17:50:54 +0800

    Adding CheckAvailability method

--- lib/atleisure/api.rb
+++ lib/atleisure/api.rb
@@ -7,8 +7,8 @@ module Atleisure
 
     def get_properties
       retry_times(3) do
-        @client = Jimson::Client.new("https://listofhousesv1.jsonrpc-partner.net/cgi/lars/jsonrpc-partner/jsonrpc.htm")
-        result = @client.ListOfHousesV1(credentials)
+        client = Jimson::Client.new("https://listofhousesv1.jsonrpc-partner.net/cgi/lars/jsonrpc-partner/jsonrpc.htm")
+        result = client.ListOfHousesV1(credentials)
       end
     end
 
@@ -17,13 +17,14 @@ module Atleisure
 
       result = nil
       time = Benchmark.measure do
-        @client = Jimson::Client.new("https://dataofhousesv1.jsonrpc-partner.net/cgi/lars/jsonrpc-partner/jsonrpc.htm")
-        result = @client.DataOfHousesV1({
+        client = Jimson::Client.new("https://dataofhousesv1.jsonrpc-partner.net/cgi/lars/jsonrpc-partner/jsonrpc.htm")
+        result = client.DataOfHousesV1({
           'HouseCodes' => identifiers,
-          'Items' => items}.merge(credentials))
+          'Items' => items}.merge(credentials)
+        )
       end
       time_ms = "%.2f" % (time.real * 1000)
-      #Ingest::Base.say "[#{time_ms}ms] Fetching properties data (#{identifiers[0..10].join(', ')})"
+      #puts "[#{time_ms}ms] Fetching properties data (#{identifiers[0..10].join(', ')})"
 
       result
     end
@@ -50,10 +51,31 @@ module Atleisure
       @layout_items ||= begin
         #puts "Fetching layout items"
         retry_times(3) do
-          @client = Jimson::Client.new("https://referencelayoutitemsv1.jsonrpc-partner.net/cgi/lars/jsonrpc-partner/jsonrpc.htm")
-          result = @client.ReferenceLayoutItemsV1(credentials)
+          client = Jimson::Client.new("https://referencelayoutitemsv1.jsonrpc-partner.net/cgi/lars/jsonrpc-partner/jsonrpc.htm")
+          result = client.ReferenceLayoutItemsV1(credentials)
         end
       end
+    end
+
+    def check_availability(house_code, start_date, end_date, price = nil)
+      client = Jimson::Client.new("https://checkavailabilityv1.jsonrpc-partner.net/cgi/lars/jsonrpc-partner/jsonrpc.htm")
+
+      params = {
+        'HouseCode' => house_code,
+        'ArrivalDate' => start_date.to_date.to_s,
+        'DepartureDate' => end_date.to_date.to_s,
+        'Price' => price || ''
+      }
+
+      raw_result = client.CheckAvailabilityV1(params.merge(credentials))
+      result = {
+        available: raw_result['Available'] == 'Yes' && raw_result['OnRequest'] == 'No'
+      }
+      if result[:available]
+        result[:price] = raw_result['CorrectPrice'].to_i
+        result[:price_message] = raw_result['PriceMessage']
+      end
+      result
     end
 
     protected
--- lib/atleisure/version.rb
+++ lib/atleisure/version.rb
@@ -1,3 +1,3 @@
 module Atleisure
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
